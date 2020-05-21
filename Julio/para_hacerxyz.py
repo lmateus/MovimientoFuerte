4dcfb6074ef75e032ba5d9bd7b05aeac4ce2b665
lmateus <lsmateusb@gmail.com>
2020-05-21 12:11:03 -0500

    Julio2

--- Julio/para_hacerxyz.py
+++ Julio/para_hacerxyz.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 #df = pd.read_csv('dem_all_z18.txt', sep=';', decimal=',', header=0, names=['FID', 'pointid', 'grid_code', 'X', 'Y'])
 df = pd.read_csv('UTM_Z18_30m.txt', sep=';', decimal=',', header=0)
-#print(df.astype(float))
+df = df.astype(float)
 '''
 pd.to_numeric(df['FID']) # Convertir de texto a float la columna
 pd.to_numeric(df['pointid'])
